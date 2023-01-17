af4cefd6558cf918ba298639fee3f859f9c47003
bholmesdev <bholmesdev@gmail.com>
2023-01-16 19:02:03 -0500

    feat: ADSR slider that sounds not bad!

--- src/Index.tsx
+++ src/Index.tsx
@@ -1,54 +1,61 @@
+import type { JSX } from "solid-js";
 import { render } from "solid-js/web";
-import { createSignal, For } from "solid-js";
+import { For } from "solid-js";
 import { createStore } from "solid-js/store";
+import "./styles.css";
+import { DETUNE, FREQUENCIES, STAGE_MAX_TIME } from "./~consts";
 
 const actx = new AudioContext();
 if (!actx) throw new Error("Trash");
 
-function createOscillator(frequency: number) {
+function createOscillator(frequency: number): Osc {
+  const [adsr] = adsrStore;
+  const gainNode = actx.createGain();
+  gainNode.connect(actx.destination);
+
   const osc = actx.createOscillator();
   osc.type = "sine";
   osc.frequency.value = frequency;
-  osc.connect(actx.destination /* speakers */);
+  osc.connect(gainNode);
   osc.start();
-  return osc;
+
+  const now = actx.currentTime;
+  const atkDuration = adsr.attack * STAGE_MAX_TIME;
+  const atkEnd = now + atkDuration;
+  const decDuration = adsr.decay * STAGE_MAX_TIME;
+
+  gainNode.gain.cancelScheduledValues(actx.currentTime);
+  gainNode.gain.setValueAtTime(0, now);
+  gainNode.gain.linearRampToValueAtTime(0.5, atkEnd);
+  gainNode.gain.setTargetAtTime(adsr.sustain, atkEnd, decDuration);
+  return {
+    osc,
+    off() {
+      const now = actx.currentTime;
+      const relDuration = adsr.release * STAGE_MAX_TIME;
+      const relEnd = now + relDuration;
+
+      gainNode.gain.cancelScheduledValues(now);
+      gainNode.gain.linearRampToValueAtTime(0, relEnd);
+      // gainNode.disconnect();
+    },
+  };
 }
 
-const FREQUENCIES = {
-  C4: {
-    frequency: 261.63,
-    keyboardKey: "a",
-  },
-  D4: {
-    frequency: 293.66,
-    keyboardKey: "s",
-  },
-  E4: {
-    frequency: 329.63,
-    keyboardKey: "d",
-  },
-  F4: {
-    frequency: 349.23,
-    keyboardKey: "f",
-  },
-  G4: {
-    frequency: 392.0,
-    keyboardKey: "g",
-  },
-  A4: {
-    frequency: 440.0,
-    keyboardKey: "h",
-  },
-  B4: {
-    frequency: 493.88,
-    keyboardKey: "j",
-  },
+type Osc = {
+  osc: OscillatorNode;
+  off: () => void;
 };
 
-const DETUNE = 0;
+const adsrStore = createStore({
+  attack: 0.01,
+  decay: 0.1,
+  sustain: 0.2,
+  release: 0.1,
+});
 
 function Key(props: { label: string; frequency: number; keyboardKey: string }) {
-  const [oscsPlaying, setOscsPlaying] = createStore<OscillatorNode[]>([]);
+  const [oscsPlaying, setOscsPlaying] = createStore<Osc[]>([]);
 
   function play() {
     if (oscsPlaying.length) return;
@@ -62,7 +69,8 @@ function Key(props: { label: string; frequency: number; keyboardKey: string }) {
   }
   function pause() {
     setOscsPlaying((oscs) => {
-      oscs.forEach((osc) => osc.stop());
+      oscs.forEach((osc) => osc.off());
+      // TODO: await oscs to stop
       return [];
     });
   }
@@ -83,11 +91,62 @@ function Key(props: { label: string; frequency: number; keyboardKey: string }) {
 
 function Index() {
   return (
-    <For each={Object.entries(FREQUENCIES)}>
-      {([label, { frequency, keyboardKey }]) => (
-        <Key label={label} frequency={frequency} keyboardKey={keyboardKey} />
-      )}
-    </For>
+    <>
+      <For each={Object.entries(FREQUENCIES)}>
+        {([label, { frequency, keyboardKey }]) => (
+          <Key label={label} frequency={frequency} keyboardKey={keyboardKey} />
+        )}
+      </For>
+      <Controls />
+    </>
+  );
+}
+
+function Controls() {
+  const [adsr, setAdsr] = adsrStore;
+  return (
+    <div>
+      <Range
+        label="ATK"
+        input={{
+          value: adsr.attack,
+          onInput: (e) => setAdsr({ attack: e.currentTarget.valueAsNumber }),
+        }}
+      />
+      <Range
+        label="DEC"
+        input={{
+          value: adsr.decay,
+          onInput: (e) => setAdsr({ decay: e.currentTarget.valueAsNumber }),
+        }}
+      />
+      <Range
+        label="SUS"
+        input={{
+          value: adsr.sustain,
+          onInput: (e) => setAdsr({ sustain: e.currentTarget.valueAsNumber }),
+        }}
+      />
+      <Range
+        label="REL"
+        input={{
+          value: adsr.release,
+          onInput: (e) => setAdsr({ release: e.currentTarget.valueAsNumber }),
+        }}
+      />
+    </div>
+  );
+}
+
+function Range(props: {
+  label: string;
+  input: JSX.HTMLAttributes<HTMLInputElement> & { value: number };
+}) {
+  return (
+    <label>
+      {props.label}
+      <input type="range" min="0" max="1" step="0.01" {...props.input} />
+    </label>
   );
 }
 
---
+++ src/~consts.ts
@@ -0,0 +1,34 @@
+export const STAGE_MAX_TIME = 2; // seconds
+
+export const FREQUENCIES = {
+  C4: {
+    frequency: 261.63,
+    keyboardKey: "a",
+  },
+  D4: {
+    frequency: 293.66,
+    keyboardKey: "s",
+  },
+  E4: {
+    frequency: 329.63,
+    keyboardKey: "d",
+  },
+  F4: {
+    frequency: 349.23,
+    keyboardKey: "f",
+  },
+  G4: {
+    frequency: 392.0,
+    keyboardKey: "g",
+  },
+  A4: {
+    frequency: 440.0,
+    keyboardKey: "h",
+  },
+  B4: {
+    frequency: 493.88,
+    keyboardKey: "j",
+  },
+};
+
+export const DETUNE = 0;
